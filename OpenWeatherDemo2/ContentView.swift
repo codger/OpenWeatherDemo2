ee1d3e21f6442392fd6354ad404c6575daa83d6d
John James <jjames@pacbell.net>
2020-09-07 20:57:37 -0700

    Initial Commit

---
+++ OpenWeatherDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenWeatherDemo2
+//
+//  Created by John James Retina on 9/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenWeatherDemo2/OpenWeatherDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherDemo2App.swift
+//  OpenWeatherDemo2
+//
+//  Created by John James Retina on 9/7/20.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
